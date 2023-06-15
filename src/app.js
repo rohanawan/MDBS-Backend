f1d50d802cdf71d47bb38b8d32bb8533bfc88b12
rohanawan <rohan.awan863@gmail.com>
2023-06-16 04:18:24 +0500

    Subject: Enable Cors

--- src/app.js
+++ src/app.js
@@ -21,6 +21,15 @@ if (config.env !== 'test') {
   app.use(morgan.errorHandler);
 }
 
+// Enable CORS
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+  next();
+});
+
 // set security HTTP headers
 app.use(helmet());
 
